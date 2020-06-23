d8f3d3192eb982fa841c6d99f7ddea2fda6a4f09
Jsteel3 <43678543+Jsteel3@users.noreply.github.com>
2020-06-23 14:55:01 -0400

    Connection Singleton and MyProps

--- src/main/java/com/revature/dmv/web/ConnectionService.java
+++ src/main/java/com/revature/dmv/web/ConnectionService.java
@@ -1,4 +1,45 @@
 package com.revature.dmv.web;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionService {
+
+
+    public static Connection connection;
+
+    public static void initialize() {
+        try  {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(MyProps.url, MyProps.name, MyProps.password);
+
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        if(connection == null){
+            initialize();
+        }
+        return connection;
+    }
+
+    @Override
+    public void finalize() {
+        try {
+            connection.close();
+        } catch(Exception e) {
+
+        }
+    }
+
+}
+
+
+
+
+
+
 }
