b40b19f8a8ae7e802fcf7bb975e4fea95a30fcac
miguelangello96 <miguelangello96@gmail.com>
2020-06-23 15:45:48 -0400

    Added DAO

--- src/main/java/com/revature/dmv/dao/IPersonDAO.java
+++ src/main/java/com/revature/dmv/dao/IPersonDAO.java
@@ -1,4 +1,13 @@
 package com.revature.dmv.dao;
 
+import com.revature.dmv.model.Person;
+
+import java.util.ArrayList;
+
 public interface IPersonDAO {
+
+    public Person addPerson(Person person);
+    public ArrayList<ArrayList<String>> getAllPeople();
+
+
 }
