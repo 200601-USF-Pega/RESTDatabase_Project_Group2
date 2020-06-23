0096014cb6d0e918857dbc37e5fcf55fb318ee92
Jsteel3 <43678543+Jsteel3@users.noreply.github.com>
2020-06-23 15:00:20 -0400

    Connection Singleton and MyProps

--- src/main/java/com/revature/dmv/web/MyProps.java
+++ src/main/java/com/revature/dmv/web/MyProps.java
@@ -5,6 +5,7 @@ public class MyProps {
     public static String url = "jdbc:postgresql://drona.db.elephantsql.com:5432/pelefve";
     public static String name = "pelefve";
     public static String password = "YabO1-tY1u781YXgREQK21TMwS2fRC9";
+    String sorry;
 
 
 
